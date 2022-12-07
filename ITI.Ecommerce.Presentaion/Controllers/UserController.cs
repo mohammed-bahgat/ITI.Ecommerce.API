b200972377152c178d9fcf6819ade2c29fe39c2d
mohamed bahgat <mhmd.bahgat0@gmail.com>
2022-12-08 01:21:07 +0200

    User Controller

---
+++ ITI.Ecommerce.Presentaion/Controllers/UserController.cs
@@ -0,0 +1,90 @@
+﻿using DTOs;
+using ITI.Ecommerce.Models;
+using ITI.Ecommerce.Presentaion.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ITI.Ecommerce.Presentaion.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        UserManager<Customer> UserManager;
+        SignInManager<Customer> SignInManager;
+        RoleManager<IdentityRole> RoleManager;
+
+        public UserController(UserManager<Customer> _UserManager,
+            SignInManager<Customer> _SignInManager,
+            RoleManager<IdentityRole> _roleManager
+        )
+         {
+            UserManager = _UserManager;
+            SignInManager = _SignInManager;
+            RoleManager = _roleManager; 
+           
+        }
+
+
+        [HttpPost("SignUp")]
+        public async Task<IActionResult> SignUp(UserCreateModel model)
+        {
+            DateTime Date = DateTime.Now;
+          
+            Customer user = new Customer()
+            {
+                UserName = model.UserName,
+                Email = model.Email,
+                Address = model.Address,
+                MobileNumber = model.MobileNumber
+                   ,
+                DateEntered = Date
+            };
+
+            IdentityResult result
+                     = await UserManager.CreateAsync(user, model.Password);
+            if (result.Succeeded == false)
+            {
+                return BadRequest(result.Errors);
+            }
+            else
+            {
+                await UserManager.AddToRoleAsync(user,"User");
+                
+                return Ok("Done");
+            }
+
+            
+        }
+        [HttpPost("SignIn")]
+        public async Task<IActionResult> SignIn(LoginModel model)
+        {
+            
+                var user = await UserManager.FindByEmailAsync(model.Email);
+
+            if (user == null)
+            {
+                return NotFound("Not Found User");
+            }
+            else
+            {
+                var result = await SignInManager.CheckPasswordSignInAsync(user, model.Password, false);
+                if (result.Succeeded)
+                {
+                    var loggedUser = new LoggedInModel()
+                    {
+                        Id=user.Id,
+                        UserName=user.UserName
+                    };
+                    return Ok(loggedUser);
+                }
+                else
+                {
+                    return NotFound("Not Valid Email Or Password");
+                }
+            }
+
+          
+        }
+    }
+}
--- ITI.Ecommerce.Presentaion/Program.cs
+++ ITI.Ecommerce.Presentaion/Program.cs
@@ -1,4 +1,5 @@
 using ITI.Ecommerce.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,6 +11,18 @@ builder.Services.AddControllers();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddIdentity<Customer, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    options.User.AllowedUserNameCharacters = string.Empty;
+    options.Password.RequireDigit = false;
+    options.Password.RequireLowercase = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequireNonAlphanumeric = false;
+});
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddCors();
