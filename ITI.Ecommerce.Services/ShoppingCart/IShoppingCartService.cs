fb8ca231a4813dd87b4f2a33d93bfbb49ee2c1a2
Ahmed <Ahmed@AHMED-PC>
2022-12-10 19:44:52 +0200

    Shopping Cart API

--- ITI.Ecommerce.Services/ShoppingCart/IShoppingCartService.cs
+++ ITI.Ecommerce.Services/ShoppingCart/IShoppingCartService.cs
@@ -5,9 +5,12 @@ namespace ITI.Ecommerce.Services
     public interface IShoppingCartService
     {
         Task add(ShoppingCartDto shoppingCartDto);
+        //Task add(ShoppingCartDT shoppingCartDT);
         Task<IEnumerable<ShoppingCartDto>> GetAll();
         Task<ShoppingCartDto> GetById(int id);
         void Delete(ShoppingCartDto shoppingCartDto);
+        void DeleteCart(int id);
         void Update(ShoppingCartDto shoppingCartDto);
+        void UpdateCart(int id , ShoppingCartDto shoppingCartDto);
     }
 }
